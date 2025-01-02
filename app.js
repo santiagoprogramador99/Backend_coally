5b80e08ab8dbfff7f0cf506c03c29edbbaf9a1bd
santiagoprogramador99 <santi.maestro99@gmail.com>
2025-01-01 19:13:44 -0500

    url 2

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-require('dotenv').config(); 
+require('dotenv').config();  
 const express = require('express');
 const cors = require('cors');
 const taskRoutes = require('./routes/taskRoutes');
@@ -6,6 +6,9 @@ const errorHandler = require('./middlewares/errorHandler');
 const swaggerUi = require('swagger-ui-express');
 const swaggerJSDoc = require('swagger-jsdoc');
 const app = express();
+
+// Configuración del puerto
+const port = process.env.PORT || 5000;
 
 // Swagger definition
 const swaggerOptions = {
@@ -19,8 +22,8 @@ const swaggerOptions = {
         servers: [
             {
                 url: process.env.NODE_ENV === 'production' 
-                    ? `https://backend-coally-1.onrender.com`  // Cambia esta URL a la de tu aplicación en Render
-                    : `http://localhost:${process.env.PORT || 5000}`,  // Esto será para tu entorno local
+                    ? `https://backend-coally-1.onrender.com`  // Cambia esta URL a la URL de tu aplicación en Render
+                    : `http://localhost:${port}`,  // Esto será para tu entorno local
             },
         ],
         components: {
@@ -38,10 +41,16 @@ const swaggerOptions = {
 
 const swaggerDocs = swaggerJSDoc(swaggerOptions);
 
+// Middleware de configuración
 app.use(cors());
 app.use(express.json());
 app.use('/api/tasks', taskRoutes);
 app.use(errorHandler);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+// Iniciar el servidor
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
 
 module.exports = app;
